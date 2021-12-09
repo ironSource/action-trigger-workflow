37019ef847553b6f003515917326b5870e6f44fd
AndyKIron <andyk@is.com>
2021-12-09 10:04:43 +0200

    chore: code-review changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,11 +72,11 @@ trigger_workflow() {
   echo "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches"
   echo "{\"ref\":\"${ref}\",\"inputs\":${inputs}}"
 
-  trigger_workflow=$(curl --fail -X POST "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
-    --data "{\"ref\":\"${ref}\",\"inputs\":${inputs}}")
+#  trigger_workflow=$(curl --fail -X POST "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
+#    -H "Accept: application/vnd.github.v3+json" \
+#    -H "Content-Type: application/json" \
+#    -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
+#    --data "{\"ref\":\"${ref}\",\"inputs\":${inputs}}")
 
   echo "Waiting ${wait_interval} seconds until triggered workflow starts"
   sleep $wait_interval
@@ -156,14 +156,14 @@ wait_for_workflow_to_finish() {
 main() {
   validate_args
 
-  trigger_workflow
-
-  if [ "${wait_workflow}" = true ]
-  then
-    wait_for_workflow_to_finish
-  else
-    echo "Skipping waiting for workflow."
-  fi
+#  trigger_workflow
+#
+#  if [ "${wait_workflow}" = true ]
+#  then
+#    wait_for_workflow_to_finish
+#  else
+#    echo "Skipping waiting for workflow."
+#  fi
 }
 
 main
